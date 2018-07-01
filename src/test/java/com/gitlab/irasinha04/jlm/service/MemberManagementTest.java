f0b8887593dca4bef6e1f979e55217b54931f219
irasinha04 <irasinha.04@gmail.com>
2018-07-01 16:55:37 +0530

    Create costants

--- src/test/java/com/gitlab/irasinha04/jlm/service/MemberManagementTest.java
+++ src/test/java/com/gitlab/irasinha04/jlm/service/MemberManagementTest.java
@@ -2,26 +2,24 @@ package com.gitlab.irasinha04.jlm.service;
 
 import com.gitlab.irasinha04.jlm.Member;
 
-//import com.gitlab.irasinha04.jlm.Member;
-
 public class MemberManagementTest {
 	
+	private static final String PHONE_NUM = "9560084813";
+	private static final String EMAIL = "irasinha.04@gmail.com";
+	private static final String NAME = "Ira";
+
 	public static void test(String actual, String expected, String variable) {
 	    System.out.println(variable + " Test " + (actual.equals(expected)? "Passed" : "Failed"));
 	}
 	
 	public static void main(String[] args) {
 		MemberManagementService test = new MemberManagementService();
-		int id = test.createMember("Ira", "irasinha.04@gmail.com", "9560084813");
+		
+		int id = test.createMember(NAME, EMAIL, PHONE_NUM);
 		Member m = test.readMember(id);
-		
-		test("Ira", m.getName(), "Name");
-		test("irasinha.04@gmail.com", m.getEmail(), "Email");
-		test("9560084813", m.getPhoneNo(), "Phone No.");
-		
-//		System.out.println("Member created");
-//		System.out.println("Details of member :");
-//		System.out.println(m);
-//		System.out.println(m.toString());
+
+		test(NAME, m.getName(), "Name");
+		test(EMAIL, m.getEmail(), "Email");
+		test(PHONE_NUM, m.getPhoneNo(), "Phone No.");
 	}
 }
