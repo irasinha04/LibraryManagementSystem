cb2628c547aaa09f37b17127c9462e53798309a9
irasinha04 <irasinha.04@gmail.com>
2018-07-01 16:46:19 +0530

    create a test method for combined testing

--- src/test/java/com/gitlab/irasinha04/jlm/service/MemberManagementTest.java
+++ src/test/java/com/gitlab/irasinha04/jlm/service/MemberManagementTest.java
@@ -5,16 +5,19 @@ import com.gitlab.irasinha04.jlm.Member;
 //import com.gitlab.irasinha04.jlm.Member;
 
 public class MemberManagementTest {
+	
+	public static void test(String actual, String expected, String variable) {
+	    System.out.println(variable + " Test " + (actual.equals(expected)? "Passed" : "Failed"));
+	}
+	
 	public static void main(String[] args) {
 		MemberManagementService test = new MemberManagementService();
 		int id = test.createMember("Ira", "irasinha.04@gmail.com", "9560084813");
 		Member m = test.readMember(id);
 		
-		System.out.println("Name test " + (m.getName().equals("Ira") ? "Passed" : "Failed"));
-		
-		System.out.println("Email test " + (m.getEmail().equals("irasinha.04@gmail.com") ? "Passed" : "Failed"));
-		
-		System.out.println("Phone no. test " + (m.getPhoneNo().equals("9560084813") ? "Passed" : "Failed"));
+		test("Ira", m.getName(), "Name");
+		test("irasinha.04@gmail.com", m.getEmail(), "Email");
+		test("9560084813", m.getPhoneNo(), "Phone No.");
 		
 //		System.out.println("Member created");
 //		System.out.println("Details of member :");
